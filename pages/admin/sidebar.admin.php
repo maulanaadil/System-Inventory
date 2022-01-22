88a4a204a379a16c413358c5365115447ab0bd52
rifkikautsar <rifkikautsar1@gmail.com>
2022-01-23 02:06:22 +0700

    link ke profile

--- pages/admin/sidebar.admin.php
+++ pages/admin/sidebar.admin.php
@@ -31,8 +31,8 @@
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="navbar-brand-wrapper d-flex justify-content-center">
                 <div class="navbar-brand-inner-wrapper d-flex justify-content-center align-items-center w-100">
-                    <a class="navbar-brand brand-logo" href="./index.php"><img src="../../images/logo.png" class="img-fluid w-100"
-                            alt="logo" /></a>
+                    <a class="navbar-brand brand-logo" href="./index.php"><img src="../../images/logo.png"
+                            class="img-fluid w-100" alt="logo" /></a>
                 </div>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
@@ -45,7 +45,7 @@
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
                             aria-labelledby="profileDropdown">
-                            <a class="dropdown-item">
+                            <a class="dropdown-item" href="profil.admin.php">
                                 <i class="mdi mdi-account-circle text-primary"></i>
                                 Profile
                             </a>
