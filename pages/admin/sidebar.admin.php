e509715fa10c83d94ab05faa870087d678b113c7
Maulana Adil <maulanaadil.latif@gmail.com>
2022-01-22 13:40:42 +0700

    tambah kondisi apabila data kosong di lupa-password-2.php

--- pages/admin/sidebar.admin.php
+++ pages/admin/sidebar.admin.php
@@ -31,7 +31,7 @@
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="navbar-brand-wrapper d-flex justify-content-center">
                 <div class="navbar-brand-inner-wrapper d-flex justify-content-center align-items-center w-100">
-                    <a class="navbar-brand brand-logo" href="#"><img src="../../images/logo.png" class="img-fluid w-100"
+                    <a class="navbar-brand brand-logo" href="./index.php"><img src="../../images/logo.png" class="img-fluid w-100"
                             alt="logo" /></a>
                 </div>
             </div>
